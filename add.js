9a444f3a98f46c730a089017b771ad08e3a1175b
Mokiy <92001109+MokiyCodes@users.noreply.github.com>
2022-12-09 06:40:33 +0100

    Create add.js

---
+++ add.js
@@ -0,0 +1,11 @@
+// Prompt: Write overly-complex JavaScript code that simply adds 2 integers
+function add(x, y) {
+  let result = 0;
+  for (let i = 0; i < y; i++) {
+    result++;
+  }
+  for (let i = 0; i < x; i++) {
+    result++;
+  }
+  return result;
+}
